59dbc6359a812b072c91d3c3f8cff6dcdc15aca8
Reikis-oss <73290731+Reikis-oss@users.noreply.github.com>
2022-11-27 12:29:52 +0300

    Анимация активных ссылок в меню

--- src/js/myScript.js
+++ src/js/myScript.js
@@ -1,4 +1,36 @@
-'use strict';
+$(document).ready(function(){
+    
+     $(window).scroll(() => {
+               let scrollDistance = $(window).scrollTop();
+
+
+               $('.section').each((i, el) => {
+
+                   if ($(el).offset().top - $('nav').outerHeight() <= scrollDistance) {
+                       $('nav a').each((i, el) => {
+                           if ($(el).hasClass('active')) {
+                               $(el).removeClass('active');
+                           }
+                       });
+
+                       $('nav li:eq(' + i + ')').find('a').addClass('active');
+                   }
+
+               });
+           });
+    
+    
+});
+
+
+
+
+
+
+
+
+
+/* 'use strict';
 
 let typeSite, 
     designSite, 
